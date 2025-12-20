e5d3f8b84b46426f78f0a9b37bb56b51fa90c7e1
Eduardo Andrade <eduardrade0224@gmail.com>
2025-12-20 08:27:21 -0500

    Initial Commit

---
+++ Inku/InkuApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkuApp.swift
+//  Inku
+//
+//  Created by Eduardo Andrade on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct InkuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
